973be0b646230c3c984dd5576a37f749fc5747ad
Saul Torres <sault1000@gmail.com>
2017-11-29 14:27:42 -0600

    Expandable Tabs and better formatting
    This was annoyingly hard to do but worth it.

--- client/js/controllers/ConventionsController.js
+++ client/js/controllers/ConventionsController.js
@@ -3,14 +3,23 @@ app.controller('ConventionsController',function($scope){
 	$scope.conventions = [{
 		name : "First Convention",
 		location : "here",
-		description : "short Discription\<br\> With second line",
+		description : "short Discription of a simple convention",
 		image: "/assets/convention.jpg"
 	},{
 		name : "Second Convention",
 		location : "There",
-		description : "short Discription\<br\> With second line\<br\> and a third",
+		description : "short Discription jfa lha difjlasiafln flasjdfisj la ldijfasld saljl laisdjfl asdiia ldfia sjflidsjf lakhsdila l",
+		image: "/assets/convention.jpg"
+	},{
+		name : "Second Convention 2",
+		location : "There",
+		description : "short Discription jfa lha difjlasiafln flasjdfisj la ldijfasld saljl laisdjfl asdiia ldfia sjflidsjf lakhsdila l",
 		image: "/assets/convention.jpg"
 	}];
 	$scope.data = [ "Item 1", "Item 2", "Item 3", "Item 4"];
-	$scope.toggle = {};
+	$scope.collapse = [];
+	$scope.toggle = function(name){
+		$scope.collapse[name] = !($scope.collapse[name]);
+	};
+	$scope.toggleCollapse = true;
 });
